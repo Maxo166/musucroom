b0f87a1d5cac62679ed41335443b7b2286004a28
Maxo166 <sallahmad059@gmail.com>
2022-05-13 14:42:41 +0200

    Fix react router not working

--- musicontroller/frontend/src/componenets/App.js
+++ musicontroller/frontend/src/componenets/App.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
-import { render } from 'react-dom'
+import { render } from 'react-dom';
+import Home from './Home';
+import CreateRoom from './CreateRoom';
+import RoomJoin from './RoomJoin';
+import {  
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 export default class App extends Component {
     constructor(props) {
@@ -7,7 +15,15 @@ export default class App extends Component {
     }
     state = {  }
     render() { 
-        return <h1>hello maxo sall</h1>;
+        return (
+            <Router>
+                <Routes>
+                    <Route path='/' element={<Home /> }/>
+                    <Route path='/create' element={<CreateRoom /> }/>
+                    <Route path='/join' element={<RoomJoin /> }/>
+                </Routes>
+            </Router>            
+            );
     }
 }
 render(<App/>, document.getElementById('app'))
